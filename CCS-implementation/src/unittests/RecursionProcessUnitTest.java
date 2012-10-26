a8d261ecfc30b5e5c815ab160c0482b3b8f5f1f5
Massimo Nocentini <massimo.nocentini@gmail.com>
2012-10-26 08:43:58 +0200

    Wrote a test with a mixture of static and dynamic operator and no redundance is produced.

--- CCS-implementation/src/unittests/RecursionProcessUnitTest.java
+++ CCS-implementation/src/unittests/RecursionProcessUnitTest.java
@@ -11,6 +11,7 @@ import processes.ParallelCompositionCCSProcess;
 import processes.RecursionCSSProcess;
 import processes.RecursionPlaceHolderCCSProcess;
 import processes.RecursionPostFixingCCSProcess;
+import processes.RestrictionCCSProcess;
 import processes.TauCCSProcess;
 import processes.VisibleChoiceCCSProcess;
 import actions.CCSChannel;
@@ -50,6 +51,62 @@ public class RecursionProcessUnitTest extends CCSProcessUnitTest {
 				is("recX:((a!.tau + (b?.tau | c?.tau).X))"));
 		Assert.assertThat(secondState.getNextStates().size(), is(3));
 
+		// first recursion...
+		secondState = process.getNextStates().get(1);
+		Assert.assertThat(secondState.getAction().toString(), is("b?"));
+		Assert.assertThat(secondState.getProcess().toString(),
+				is("(tau | c?.tau).X"));
+		Assert.assertThat(secondState.getNextStates().size(), is(1));
+		secondState = secondState.getNextStates().get(0);
+		Assert.assertThat(secondState.getAction().toString(), is("c?"));
+		Assert.assertThat(secondState.getProcess().toString(),
+				is("recX:((a!.tau + (b?.tau | c?.tau).X))"));
+		Assert.assertThat(secondState.getNextStates().size(), is(3));
+
+		// ...second recursion...
+		secondState = process.getNextStates().get(1);
+		Assert.assertThat(secondState.getAction().toString(), is("b?"));
+		Assert.assertThat(secondState.getProcess().toString(),
+				is("(tau | c?.tau).X"));
+		Assert.assertThat(secondState.getNextStates().size(), is(1));
+		secondState = secondState.getNextStates().get(0);
+		Assert.assertThat(secondState.getAction().toString(), is("c?"));
+		Assert.assertThat(secondState.getProcess().toString(),
+				is("recX:((a!.tau + (b?.tau | c?.tau).X))"));
+		Assert.assertThat(secondState.getNextStates().size(), is(3));
+
+		// ...and the story repeat...
+
+	}
+
+	@Test
+	public void mixing_static_and_dynamic_operators_produce_redundance() {
+		CCSProcess process = makeProcessWithMixtureOfOperators();
+
+		Assert.assertThat(process.toString(), is("(recX:(a!.tau.X))\\{b}"));
+
+		Assert.assertThat(process.getNextStates().size(), is(1));
+
+		CCSProcessState firststate = process.getNextStates().get(0);
+		Assert.assertThat(firststate.getAction().toString(), is("a!"));
+		Assert.assertThat(firststate.getProcess().toString(),
+				is("(recX:(a!.tau.X))\\{b}"));
+
+		// first recursion...
+		Assert.assertThat(firststate.getNextStates().size(), is(1));
+		firststate = process.getNextStates().get(0);
+		Assert.assertThat(firststate.getAction().toString(), is("a!"));
+		Assert.assertThat(firststate.getProcess().toString(),
+				is("(recX:(a!.tau.X))\\{b}"));
+
+		// ...second recursion...
+		Assert.assertThat(firststate.getNextStates().size(), is(1));
+		firststate = process.getNextStates().get(0);
+		Assert.assertThat(firststate.getAction().toString(), is("a!"));
+		Assert.assertThat(firststate.getProcess().toString(),
+				is("(recX:(a!.tau.X))\\{b}"));
+		Assert.assertThat(firststate.getNextStates().size(), is(1));
+
 		// and the story repeat...
 
 	}
@@ -71,6 +128,23 @@ public class RecursionProcessUnitTest extends CCSProcessUnitTest {
 						"a")), new TauCCSProcess()),
 				recursionPostFixingCCSProcess);
 		return new RecursionCSSProcess(bodyOfRecursion, placeHolder);
+
+	}
+
+	protected CCSProcess makeProcessWithMixtureOfOperators() {
+		String placeHolderName = "X";
+
+		RecursionPlaceHolderCCSProcess placeHolder = new RecursionPlaceHolderCCSProcess(
+				placeHolderName);
+
+		CCSProcess bodyOfRecursion = new RecursionPostFixingCCSProcess(
+				new ActionPrefixingCCSProcess(new OutputAction(new CCSChannel(
+						"a")), new TauCCSProcess()), placeHolder);
+
+		CCSProcess recursionProcess = new RecursionCSSProcess(bodyOfRecursion,
+				placeHolder);
+
+		return new RestrictionCCSProcess(recursionProcess, new CCSChannel("b"));
 
 	}
 }
